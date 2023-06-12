8d2fd17a7a973db4393aaa19b7be705721a5db7a
Phoomiphat Charuensab <phoomiphat@vekin.co.th>
2023-06-12 17:41:12 +0700

    clean comment

--- main.go
+++ main.go
@@ -16,15 +16,4 @@ func main() {
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 
-	// r := gin.Default()
-	// config.InitConfig()
-	// text := viper.Get("DB_HOST")
-
-	// r.GET("/", func(c *gin.Context) {
-	// 	c.JSON(200, gin.H{
-	// 		"message": text,
-	// 	})
-	// })
-
-	// r.Run(":3000") // Start the Gin server
 }
