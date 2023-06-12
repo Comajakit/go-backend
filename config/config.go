c4c0f8f3596a9f72d800bca3e8089649b13976de
Phoomiphat Charuensab <phoomiphat@vekin.co.th>
2023-06-12 14:44:53 +0700

    test2

--- config/config.go
+++ config/config.go
@@ -6,7 +6,7 @@ import (
 
 func InitConfig() {
 	viper.SetConfigName("config")
-	viper.SetConfigType("yaml")
+	viper.SetConfigType("yamml")
 	viper.AddConfigPath(".")
 
 	err := viper.ReadInConfig()
