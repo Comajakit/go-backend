b8e5a2654a11f73c16659a6c3389e4c9fdbf6b7f
Phoomiphat Charuensab <phoomiphat@vekin.co.th>
2023-06-12 17:25:37 +0700

    complete sample api

--- main.go
+++ main.go
@@ -1,17 +1,30 @@
 package main
 
 import (
-	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"pokemon/config"
+	db "pokemon/database"
+	"pokemon/routers"
 )
 
 func main() {
-	r := gin.Default()
+	config.InitConfig()
+	db.InitDB()
 
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Hello, Gin!",
-		})
-	})
+	router := routers.SetupRouter()
 
-	r.Run(":3000") // Start the Gin server
+	log.Fatal(http.ListenAndServe(":3000", router))
+
+	// r := gin.Default()
+	// config.InitConfig()
+	// text := viper.Get("DB_HOST")
+
+	// r.GET("/", func(c *gin.Context) {
+	// 	c.JSON(200, gin.H{
+	// 		"message": text,
+	// 	})
+	// })
+
+	// r.Run(":3000") // Start the Gin server
 }
